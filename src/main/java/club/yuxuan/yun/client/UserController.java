8c6a934a44f09b4a661dcdc3e6942a7b5b59f40e
yuxuanjava <han630389657@vip.qq.com>
2019-12-04 17:38:14 +0800

    FeignClient

--- src/main/java/club/yuxuan/yun/client/UserController.java
+++ src/main/java/club/yuxuan/yun/client/UserController.java
@@ -1,14 +1,25 @@
 package club.yuxuan.yun.client;
 
+import club.yuxuan.yun.feign.test.TestDto;
+import club.yuxuan.yun.feign.test.TestFeign;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class UserController {
     
-    @RequestMapping("/")
-    public String aaa() {
-        return "succcess";
+    @Resource
+    private TestFeign testFeign;
+    
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+        TestDto dto = new TestDto();
+        dto.setParam("success");
+        String success = testFeign.printStr(dto);
+        return success;
     }
     
 }
--- src/main/java/club/yuxuan/yun/client/XuansClientApplication.java
+++ src/main/java/club/yuxuan/yun/client/XuansClientApplication.java
@@ -2,10 +2,14 @@ package club.yuxuan.yun.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableEurekaClient
+@EnableFeignClients(basePackages = "club.yuxuan.yun")
 public class XuansClientApplication {
 
     public static void main(String[] args) {
