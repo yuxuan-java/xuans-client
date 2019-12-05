a04c4aae9fe1c137ad72168e9a86bebb1b04d1a2
yuxuanjava <han630389657@vip.qq.com>
2019-12-05 10:51:19 +0800

    queryAnswerByPK

--- src/main/java/club/yuxuan/yun/client/controller/FeignTestController.java
+++ src/main/java/club/yuxuan/yun/client/controller/FeignTestController.java
@@ -1,15 +1,15 @@
 package club.yuxuan.yun.client.controller;
 
-import club.yuxuan.yun.feign.test.TestDto;
+import club.yuxuan.yun.common.base.Response;
+import club.yuxuan.yun.common.controller.BaseController;
+import club.yuxuan.yun.feign.answer.AnswerFacade;
+import club.yuxuan.yun.feign.answer.dto.AnswerReq;
+import club.yuxuan.yun.feign.answer.dto.AnswerResp;
+import club.yuxuan.yun.feign.test.dto.TestDto;
 import club.yuxuan.yun.feign.test.TestFeign;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.annotation.Resource;
@@ -22,7 +22,7 @@ import javax.annotation.Resource;
  **/
 @RestController
 @Api(value = "FeignTestController", tags = "Feign测试Controller")
-public class FeignTestController {
+public class FeignTestController extends BaseController {
 
     /**
      * House服务的测试Feign
@@ -30,24 +30,34 @@ public class FeignTestController {
     @Resource
     private TestFeign testFeign;
     
+    @Resource
+    private AnswerFacade answerFacade;
+    
     /**
      * 测试Feign调用
      *
      * @author yuxuan.han
      * @date 2019/12/4 23:08 
      * @param dto
-     * @param abc
+     * @param abc
      * @return java.lang.String
      */
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "param", value = "测试参数", dataType = "String", required = true)
-//    })
     @ApiOperation("测试Feign调用")
     @RequestMapping(value = "/test", method = RequestMethod.POST)
-    public String test(@RequestBody TestDto dto, @ApiIgnore String abc) {
+    public Response<String> test(@RequestBody TestDto dto, @ApiIgnore String abc) {
         System.out.println(abc);
         String success = testFeign.printStr(dto);
-        return success;
+        return success(success);
+    }
+    
+    @ApiOperation("根据主键查询")
+    @PostMapping("/queryAnswerByPK")
+    public Response<AnswerResp> queryAnswerByPK(@RequestParam("id") String id) {
+        Response<AnswerResp> answerRespResponse = answerFacade.selectByPrimaryKey(new AnswerReq(id));
+        if (answerRespResponse != null && answerRespResponse.getData() != null) {
+            return answerRespResponse;
+        }
+        return success();
     }
     
 }
