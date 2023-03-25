10108fb387d67ea2c979d34ce121d42b686221fd
yuxuan_java <yuxuan_java@163.com>
2023-03-25 16:55:37 +0800

    2023-03-25 16:55:00

--- src/main/java/club/yuxuan/yun/client/XuansClientApplication.java
+++ src/main/java/club/yuxuan/yun/client/XuansClientApplication.java
@@ -4,7 +4,6 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
